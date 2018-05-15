44131d99c0bd60114aee8354bda2d8a969311212
Gabriel Lasso <gabriel.lasso@usp.br>
2018-05-15 12:39:02 -0300

    Tentando fazer criar as partidas

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :partidas, class_name: "Partida"
   def initialize(param)
       super(param)
       self.elo_pm = 100;
