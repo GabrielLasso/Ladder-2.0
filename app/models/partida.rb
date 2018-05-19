04b2b23ad421c08518040753a7141cdf2b33287d
Fabio Henrique Tanaka <fhktanaka@gmail.com>
2018-05-18 23:25:05 -0300

    jogo incorporado ao sistema

--- app/models/partida.rb
+++ app/models/partida.rb
@@ -1,6 +1,3 @@
-$game1 = "Melee"
-$game2 = "Project M"
-$game3 = "Undernight"
 class Partida < ApplicationRecord
     belongs_to :player1, class_name: "User"
     belongs_to :player2, class_name: "User"
