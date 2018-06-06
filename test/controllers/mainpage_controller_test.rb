79f6ae71391e7367ec29f623b81a254f7f58ddc3
Gabriel Lasso <gabriel.lasso@usp.br>
2018-06-06 05:51:33 -0300

    Adicionada validação dos resultados e refatoração do controler das partidas
    Signed-off-by: Gabriel Lasso <gabriel.lasso@usp.br>
    Signed-off-by: @fhtanaka

--- test/controllers/mainpage_controller_test.rb
+++ test/controllers/mainpage_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class HelloworldControllerTest < ActionDispatch::IntegrationTest
+class MainpageControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
