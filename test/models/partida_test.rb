c1a0bf86a23e80483754a773eda447f836216ef1
Gabriel Lasso <gabriel.lasso@usp.br>
2018-06-07 09:39:40 -0300

    Fixed isssue 9

--- test/models/partida_test.rb
+++ test/models/partida_test.rb
@@ -1,65 +1,46 @@
 require 'test_helper'
 
 class PartidaTest < ActiveSupport::TestCase
-   test "the truth" do
-     assert true
-   end
+   #test "the truth" do
+   #  assert true
+   #end
     test "Consegue criar partida" do
-        user1 = User.new({username: "Player1", email: "player1@ladder.com", password: "123456"})
-        user1.save
-        user2 = User.new({username: "Player2", email: "player2@ladder.com", password: "123456"})
-        user2.save
-        partida = Partida.new
-        partida.player1 = user1
-        partida.player2 = user2
-        partida.game = 1
-        partida.winP1 = 3
-        partida.winP2 = 2
+        user1 = User.find(1)
+        user2 = User.find(2)
+        partida = Partida.new({player1: user1, player2: user2, game: 1, winP1: 3, winP2: 2})
         assert partida.save
     end
     test "Requer player 1" do
-        user2 = User.new({username: "Player2", email: "player2@ladder.com", password: "123456"})
-        user2.save
-        partida = Partida.new
-        partida.player2 = user2
-        partida.game = 1
-        partida.winP1 = 3
-        partida.winP2 = 2
+        user1 = User.find(1)
+        user2 = User.find(2)
+        partida = Partida.new({player2: user2, game: 1, winP1: 3, winP2: 2})
         assert_not partida.save
     end
     test "Requer player 2" do
-        user1 = User.new({username: "Player1", email: "player1@ladder.com", password: "123456"})
-        user1.save
-        partida = Partida.new
-        partida.player2 = user1
-        partida.game = 1
-        partida.winP1 = 3
-        partida.winP2 = 2
+        user1 = User.find(1)
+        user2 = User.find(2)
+        partida = Partida.new({player1: user1, game: 1, winP1: 3, winP2: 2})
         assert_not partida.save
     end
     test "Tem que ser melhor de 5" do
-        user1 = User.new({username: "Player1", email: "player1@ladder.com", password: "123456"})
-        user1.save
-        user2 = User.new({username: "Player2", email: "player2@ladder.com", password: "123456"})
-        user2.save
-        partida = Partida.new
-        partida.player1 = user1
-        partida.player2 = user2
-        partida.game = 1
-        partida.winP1 = 2
-        partida.winP2 = 2
+        user1 = User.find(1)
+        user2 = User.find(2)
+        partida = Partida.new({player1: user1, player2: user2, game: 1, winP1: 2, winP2: 2})
         assert_not partida.save
     end
     test "Requer um jogo" do
-        user1 = User.new({username: "Player1", email: "player1@ladder.com", password: "123456"})
-        user1.save
-        user2 = User.new({username: "Player2", email: "player2@ladder.com", password: "123456"})
-        user2.save
-        partida = Partida.new
-        partida.player1 = user1
-        partida.player2 = user2
-        partida.winP1 = 2
-        partida.winP2 = 3
+        user1 = User.find(1)
+        user2 = User.find(2)
+        partida = Partida.new({player1: user1, player2: user2, winP1: 3, winP2: 2})
         assert_not partida.save
+    end
+    test "Atualiza o elo" do
+        user1 = User.find(1)
+        user2 = User.find(2)
+        old_elo1 = user1.elo_1
+        old_elo2 = user2.elo_1
+        partida = Partida.new({player1: user1, player2: user2, game: 1, winP1: 3, winP2: 2})
+        partida.save
+        assert user1.elo_1 > old_elo1 and user2.elo_1 < old_elo2
     end
 end
