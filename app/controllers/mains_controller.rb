9c79a2e58cbc5504f0adeba75b418cd736fb0f1d
Gabriel Lasso <gabriel.lasso@usp.br>
2018-06-08 15:11:04 -0300

    Agora dá pra add mains
    Signed-off-by: Gabriel Lasso <gabriel.lasso@usp.br>
    Signed-off-by: @fhtanaka

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -4,14 +4,13 @@ class MainsController < ApplicationController
     end
     def delete_main
         id = params[:id]
-        personagem = Personagem.where("id=#{id}")[0]
+        personagem = Personagem.find(id)
         current_user.delete_main(personagem)
         redirect_to mains_path
     end
     def add_main
-        id = params[:id]
-        100.times {puts params}
-        personagem = Personagem.where("id=#{id}")[0]
+        id = params[:char_id]
+        personagem = Personagem.find(id)
         current_user.add_main(personagem)
         redirect_to mains_path
     end
