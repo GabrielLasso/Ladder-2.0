a2f271551476ecb286398be2cc9ce87351fd542f
glasso <gabriel.lasso@usp.br>
2018-06-20 18:21:36 -0300

    Arrumei os testes da partida

--- test/controllers/partidas_controller_test.rb
+++ test/controllers/partidas_controller_test.rb
@@ -7,17 +7,17 @@ class PartidasControllerTest < ActionDispatch::IntegrationTest
         sign_in users(:one)
     end
 
-    test "deveria pegar index" do
+    test "should get index" do
         get partidas_url
         assert_response :success
     end
 
-    test "deveria pegar nova" do
+    test "should get new" do
       get new_partida_url
       assert_response :success
     end
 
-    test "deveria criar partida" do
+    test "should create partida" do
       assert_difference('Partida.count') do
           post partidas_url, params: { partida: {player2: @partida.player2, winP1: @partida.winP1, winP2: @partida.winP2, game: @partida.game} }
       end
@@ -25,28 +25,29 @@ class PartidasControllerTest < ActionDispatch::IntegrationTest
       assert_redirected_to partida_url(Partida.last)
     end
 
-    test "deveria mostrar partida" do
+    test "should show partida" do
       get partida_url(@partida)
       assert_response :success
     end
 
-    test "deveria pegar edit" do
+    test "should get edit" do
       users(:one).admin = true
       get edit_partida_url(@partida)
       assert_response :success
     end
 
-    test "deveria update partida" do
+    test "should update partida" do
       users(:one).admin = true
-      patch partida_url(@partida), params: { partida: { winP1: @partida.winP1, winP2: @partida.winP2 } }
+      patch partida_url(@partida), params: { partida: { game: @partida.game, player1: @partida.player1, player2: @partida.player2, winP1: @partida.winP1, winP2: @partida.winP2 } }
       assert_redirected_to partida_url(@partida)
     end
 
-    #test "should destroy partida" do
-    #  assert_difference('Partida.count', -1) do
-    #    delete partida_url(@partida)
-    #  end
+    test "should destroy partida" do
+      users(:one).admin = true
+      assert_difference('Partida.count', -1) do
+        delete partida_url(@partida)
+      end
 
-    #  assert_redirected_to partidas_url
-    #end
+      assert_redirected_to partidas_url
+    end
 end
