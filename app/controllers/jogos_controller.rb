6f18bb89eeb259f5542f97a376e334f4b1a947f7
fhtanaka <fhktanaka@gmail.com>
2018-06-24 12:33:18 -0300

    Adicionada views de personagems com opções de adicionar e excluir

--- app/controllers/jogos_controller.rb
+++ app/controllers/jogos_controller.rb
@@ -17,6 +17,21 @@ class JogosController < ApplicationController
                 format.json { head :no_content }
             end
         end
-
+    end
+    def personagens
+    	@jogo = Jogo.find(params[:jogo])
+    	@personagens = Personagem.where('jogo_id = ?', params[:jogo])
+    end
+    def adiciona_personagem
+    	Personagem.create ({nome: params[:nome], jogo: Jogo.find(params[:jogo])}) unless params[:nome].blank?
+    	redirect_to lista_personagens_path(:jogo => params[:jogo])
+    end
+    def remove_personagem
+    	@personagem = Personagem.find(params[:char])
+    	@personagem.destroy
+        respond_to do |format|
+            format.html { redirect_to lista_personagens_path(:jogo => params[:jogo]), notice: 'Personagem removido com sucesso.' }
+            format.json { head :no_content }
+        end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
   get "/historico" => "historico#list_by_user", as: 'historico_user'
   get "/historico/game/:id" => "historico#list_by_game", as: 'historico_game'
   get "/games" => "jogos#index", as: 'listar_jogos'
+  get "/games/personagens/:jogo" => "jogos#personagens", as: 'lista_personagens'
+  post "games/personagens/adicionar" => "jogos#adiciona_personagem", as: 'adiciona_personagem'
+  post "games/personagens/reover" => "jogos#remove_personagem", as: 'remove_personagem'
   post "/games" => "jogos#new_game"
   post "/game/remove" => "jogos#remove_jogo", as: 'remove_jogo'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
